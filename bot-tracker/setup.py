fcb9d1465325477c91b781f885da1ac2f9bddcb6
hsuanguo <hsuan.guo@gmail.com>
2023-06-06 11:37:28 +0100

    added missing dependencies & make fastreid optional

--- bot-tracker/setup.py
+++ bot-tracker/setup.py
@@ -1,27 +1,27 @@
-#!/usr/bin/env python3
-
 from setuptools import find_packages, setup
 
 package_name = "bot-tracker"
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     # the dependency list
     dependency_list = [
         "cython",
         "numpy",
         "scipy",
+        "lap",
         "motmetrics",
-        "cython_bbox",
+        "opencv-python",
+        "cython_bbox==0.1.13",
         "matplotlib",
     ]
 
     # final setup
     setup(
         name=package_name,
-        version='0.1.0',
+        version="0.2.0",
         python_requires=">=3.8",
-        packages=find_packages(exclude=['tests*']),
+        packages=find_packages(exclude=["tests*"]),
         include_package_data=True,
         description="Bot Tracker",
         long_description="BoT-SORT: Robust Associations Multi-Pedestrian Tracking",
